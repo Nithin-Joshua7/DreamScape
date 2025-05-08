2ea18c35310c6eb5ecc160a8dfac06351e7eaf00
Nithin Joshua <nithinjoshua11@gmail.com>
2025-05-08 21:38:35 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:4000/api/",
+  baseURL:"https://dreamscape-server.onrender.com/api/",
   withCredentials: true,
 });
